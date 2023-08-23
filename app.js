7d05ef43d2fdc95ff2dc59b1d22e3fe8cc195a04
Tanush-Kaushik <141478739+Tanush-Kaushik@users.noreply.github.com>
2023-08-23 13:11:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.use(cors({
     credentials:true
 }))
 
+app.use(res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Headers", "X-Requested-With"); 
+res.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+res.setHeader("X-Powered-By",' 3.2.1');
+res.setHeader("Content-Type", "application/json;charset=utf-8");)
+
 
 mongoose.connect(process.env.MONGO_URI,{
     dbName:'backendAPI'
